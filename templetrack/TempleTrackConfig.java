6e6a85e092105ab6ae11c49d19fda06ca1376d5a
Flying477 <47866444+Flying477@users.noreply.github.com>
2019-02-21 22:59:31 +0100

    Delete TempleTrackConfig.java

--- templetrack/TempleTrackConfig.java
+++
@@ -1,9 +0,0 @@
-package net.runelite.client.plugins.templetrack;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-
-@ConfigGroup("templetrack")
-public interface TempleTrackConfig extends Config {
-}
