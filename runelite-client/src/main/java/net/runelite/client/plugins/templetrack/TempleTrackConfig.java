0942d6af4bf06be17fb002831e60727eed82f697
Flying477 <47866444+Flying477@users.noreply.github.com>
2019-02-21 23:01:35 +0100

    Add files via upload

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/templetrack/TempleTrackConfig.java
@@ -0,0 +1,9 @@
+package net.runelite.client.plugins.templetrack;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+
+@ConfigGroup("templetrack")
+public interface TempleTrackConfig extends Config {
+}
